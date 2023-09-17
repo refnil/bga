ad369d620b67f788f3b6101d72a2ef0c4386d33e
Martin Lavoie <broemartino@gmail.com>
2023-09-17 20:10:16 -0300

    Parse jsons

--- src/bga_table_status.py
+++ src/bga_table_status.py
@@ -3,10 +3,6 @@ import datetime
 import logging
 from logging.handlers import RotatingFileHandler
 
-from bga_account import BGAAccount
-from bga_game_list import get_game_list
-from bga_game_list import update_games_cache
-from creds_iface import get_discord_id
 from utils import normalize_name
 
 logging.getLogger("discord").setLevel(logging.WARN)
@@ -18,81 +14,6 @@ formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-
-
-async def get_tables_by_players(players, message, send_running_tables=True, game_target=""):
-    """Send running tables option is for integration where people don't want to see existing tables."""
-    bga_ids = []
-    tables = {}
-    bga_account = BGAAccount()
-    sent_messages = []
-    for player in players:
-        if player.startswith("<@"):
-            await message.channel.send("Not yet set up to read discord tags.")
-            bga_account.close_connection()
-            return
-        bga_id = bga_account.get_player_id(player)
-        if bga_id == -1:
-            await message.channel.send(f"Player {player} is not a valid bga name.")
-            bga_account.close_connection()
-            return
-        bga_ids.append(bga_id)
-        player_tables = bga_account.get_tables(bga_id)
-        found_msg = await message.channel.send(f"Found {str(len(player_tables))} tables for {player}")
-        sent_messages += [found_msg]
-        tables.update(player_tables)
-
-    bga_games, err_msg = get_game_list()
-    if len(err_msg) > 0:
-        await message.channel.send(err_msg)
-        return
-    normalized_bga_games = [normalize_name(game) for game in bga_games]
-    player_tables = []
-    for table_id in tables:
-        table = tables[table_id]
-        table_player_ids = table["player_display"]  # Table.player_display is the player Ids at this table
-        if set(bga_ids).issubset(table_player_ids):
-            # match the game if a game was specified
-            normalized_game_name = get_bga_alias(table["game_name"])
-            if len(game_target) == 0 or normalized_game_name == normalize_name(game_target):
-                player_tables.append(table)
-    for table in player_tables:
-        sent_messages += [await message.channel.send("Getting table information...")]
-        logger.debug(f"Checking table {table_id} for bga_ids {str(bga_ids)} in table {str(table)}")
-        # Check for game name by id as it may differ from name (i.e. 7 vs 'seven')
-        game_name_list = [game for game in bga_games if table["game_id"] == str(bga_games[game])]
-        if len(game_name_list) == 0:
-            game_name = table["game_name"]
-            new_game = {table["game_name"]: table["game_id"]}
-            normalized_bga_games.append(normalize_name(table["game_name"]))
-            update_games_cache(new_game)
-        else:
-            game_name = game_name_list[0]
-        if normalize_name(game_name) not in normalized_bga_games:
-            bga_account.close_connection()
-            await message.channel.send(f"{game_name} is not a BGA game.")
-            return
-        # Only add table status lines for games we care about
-        if len(game_target) > 0 and normalize_name(game_name) != normalize_name(game_target):
-            continue
-        if send_running_tables:
-            await send_active_tables_list(message, bga_account, table, game_name)
-    for sent_message in sent_messages:  # Only delete all status messages once we're done
-        await sent_message.delete()
-    if len(player_tables) == 0:
-        # Try to convert bga names to discord names
-        players_list = []
-        for player_name in players:
-            is_player_added = False
-            if message.guild:
-                player_id = get_discord_id(player_name, message)
-                if player_id != -1:
-                    players_list.append(f"<@!{player_id}>")
-                    is_player_added = True
-            elif not is_player_added:
-                players_list.append(player_name)
-        await message.channel.send(f"No {game_target} tables found for players [{', '.join(players_list)}].")
-    bga_account.close_connection()
 
 
 def get_bga_alias(game_name):
--- src/main.py
+++ src/main.py
@@ -1,37 +1,17 @@
 #!/usr/bin/env python3
 """Bot to create games on discord."""
+from collections import ChainMap
+import typing
+import json
+from dataclasses import dataclass, field
 import logging.handlers
-import time
-import traceback
-import shlex
+import argparse
 
-import discord
 from bga_game_list import bga_game_message_list, is_game_valid
-from bga_table_status import get_tables_by_players
 from bga_create_game import setup_bga_game
-from bga_message import send_message
-from creds_iface import setup_bga_account
-from bga_add_friend import add_friends
-from keys import TOKEN
-from tfm_create_game import init_tfm_game
-from menu_root import trigger_interactive_response
-from utils import send_help, force_double_quotes
+from utils import send_help
 
 LOG_FILENAME = "errs"
-SUBCOMMANDS = [
-    "!setup",
-    "!play",
-    "!status",
-    "!help",
-    "!options",
-    "!list",
-    "!list_games",
-    "!friend",
-    "!tfm",
-    "!purge",
-    "!message",
-    "!msg",
-]
 logger = logging.getLogger(__name__)
 logging.getLogger("discord").setLevel(logging.WARN)
 # Add the log message handler to the logger
@@ -41,164 +21,101 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-intents = discord.Intents(messages=True, guilds=True, members=True)
-client = discord.Client(intents=intents)
-# Keep track of context in global variable {"<author id>": {"context": context, "timestamp": int timestamp}}
-contexts = {}
+parser = argparse.ArgumentParser(prog="bga-utils")
+parser.add_argument('--users-path', required=True)
+parser.add_argument('--operations-path', required=True)
+parser.add_argument("--validate", default=False, action='store_true')
+parser.add_argument("--dry-run", default=False, action='store_true')
 
 
-@client.event
-async def on_ready():
-    """Let the user who started the bot know that the connection succeeded."""
-    logger.info(f"{client.user.name} has connected to Discord, and is active on {len(client.guilds)} servers!")
-    listening_to_help = discord.Activity(type=discord.ActivityType.listening, name="!help")
-    await client.change_presence(activity=listening_to_help)
+@dataclass
+class User:
+    name: str
+    password: typing.Optional[str] = None
+
+    @property
+    def has_password(self):
+        return self.password is not None
 
 
-@client.event
-async def on_message(message):
-    """Listen to messages so that this bot can do something."""
-    # Don't respond to this bot's own messages!
-    if message.author == client.user:
-        return
-    if message.content.startswith("!bga"):  # Transition to new syntax
-        message.content = message.content.replace("bga ", "").replace("make", "play").replace("tables", "status")
-        message.content = message.content.replace("bga", "help")  # If it's just !bga, do !help instead
-    if any([message.content.startswith(i) for i in SUBCOMMANDS]):
-        log_received_message(message)
-        if message.content.startswith("!tfm"):
-            await try_catch(message, init_tfm_game, [message])
-        # separate from other bga commands because we don't want to strip ' and " from message
-        elif message.content.startswith("!msg") or message.content.startswith("!message"):
-            if message.content.count(" ") >= 2:  # equivalent to checking for 3+ args
-                args = message.content.split(" ")
-                dest_author_name = args[1]
-                message_content = " ".join(args[2:])
-                ret_msg = await send_message(message.author.id, dest_author_name, message_content)
-                await message.channel.send(ret_msg)
+@dataclass
+class Operation:
+    game: str
+    toCreate: str
+    toInvite: typing.Set[str] = field(default_factory=set)
+    options: typing.Dict[str, str] = field(default_factory=dict)
+
+
+@dataclass
+class Config:
+    users_path: str
+    operations_path: str
+    validate: bool
+    dry_run: bool
+
+    def users_gen(self):
+        with open(self.users_path) as f:
+            users = json.load(f)
+            if isinstance(users, list):
+                for user in users:
+                    if isinstance(user, str):
+                        yield User(user)
+                    else:
+                        yield User(user['username'], user['password'])
+
+    def users(self):
+        return list(self.users_gen())
+
+    def operations(self):
+        errors = []
+
+        def parse_list(elems, context):
+            for elem in elems:
+                yield from parse_any(elem, context)
+
+        def parse_dict(elem, parent_context: ChainMap):
+            context = parent_context.new_child()
+
+            toCreate = elem.get("toCreate")
+            if toCreate is not None:
+                context["toCreate"] = toCreate
+
+            toInvite = elem.get("toInvite")
+            if toInvite is not None:
+                if isinstance(toInvite, str):
+                    context["toInvite"] = context.get("toInvite", []) + [toInvite]
+                elif isinstance(toInvite, list):
+                    context["toInvite"] = context.get("toInvite", []) + toInvite
+                else:
+                    errors.append(Exception("toInvite", toInvite))
+
+            options = elem.get("options")
+            if options is not None:
+                context["options"] = context.get("options", {}) | options
+
+            game = elem.get("game")
+            if game is not None:
+                yield Operation(game, **context)
+
+            yield from parse_any(elem.get("children"), context)
+
+        def parse_any(elem, context: ChainMap):
+            if elem is None:
+                pass
+            elif isinstance(elem, dict):
+                yield from parse_dict(elem, context)
+            elif isinstance(elem, list):
+                yield from parse_list(elem, context)
             else:
-                await message.channel.send(
-                    "You must specify both a user and a message like `!message friendo Let's play can't stop!`.",
-                )
-        else:
-            # Preserve command syntax and when there are missing args, go interactive
-            try:
-                # Replace quotes and strip white space to sanitize arguments
-                message.content = force_double_quotes(message.content)
-                args = shlex.split(message.content)
-                args = [arg.strip() for arg in args]
-                await try_catch(message, trigger_bga_action, [message, args])
-            except ValueError as e:
-                await message.channel.send("Problem parsing command: " + str(e))
-    # Use a contexts variable to keep track of next step for user.
-    # this can be anything the user sends to the bot and needs to be parsed according to the context.
-    elif str(message.channel.type) == "private" and message.channel.me == client.user:
-        log_received_message(message)
-        safe_to_check_timestamp = str(message.author) in contexts and "timestamp" in contexts[str(message.author)]
-        if str(message.author) not in contexts:
-            await try_catch(message, trigger_interactive_response, [message, contexts, "", []])
-        elif safe_to_check_timestamp and contexts[str(message.author)]["timestamp"] > time.time() - 300:
-            if "subcommand" in contexts[str(message.author)] and contexts[str(message.author)]["subcommand"]:
-                command = contexts[str(message.author)]["subcommand"]
-            else:
-                command = message.content.split(" ")[0][1:]  # remove leading ! for command
-            interactive_args = [message, contexts, command, []]
-            await try_catch(message, trigger_interactive_response, interactive_args)
-        elif "context" in contexts[str(message.author)]:
-            await message.channel.send(f"Operation timed out for operation {contexts[str(message.author)]['context']}")
-            await try_catch(message, trigger_interactive_response, [message, contexts, "timeout", []])
-        else:
-            await try_catch(message, trigger_interactive_response, [message, contexts, "timeout", []])
+                errors.append(Exception(elem))
+
+        with open(self.operations_path) as f:
+            ops = list(parse_any(json.load(f), ChainMap()))
+            return (ops, errors)
 
 
-def log_received_message(message):
-    """Log incoming messages, but strip passwords if possible.
+config = Config(**vars(parser.parse_args()))
 
-    Passwords are expected in the !setup longform command
-    as well as !setup when the context is `bga password`
-    """
-    is_context_bga_password = (
-        str(message.author) in contexts
-        and "context" in contexts[str(message.author)]
-        and contexts[str(message.author)] == "bga password"
-    )
-    if not is_context_bga_password:
-        msg = message.content
-        if "setup" in message.content and len(message.content.split(" ")) >= 4:
-            args = msg.split(" ")[:-1].append("[obfuscated password]")
-            msg = " ".join(args)  # Don't log passwords, which should be last arg
-        if message.guild:
-            guild = "guild " + message.guild.name
-        else:
-            guild = "private message"
-        logger.debug(
-            f"Received message `{msg}` from {message.author.name} (ID:{message.author.id}) via {guild}",
-        )
-
-
-async def try_catch(message, function, args_list):
-    try:
-        await function(*args_list)
-    except discord.errors.Forbidden as e:
-        if e.text == "403 Forbidden (error code: 50013): Missing Permissions":
-            await message.channel.send(
-                "The operation failed because admins didn't give the bot the permissions it needs on this server. Try kicking and readding the bot.",
-            )
-        elif e.text == "403 Forbidden (error code: 50007): Cannot send messages to this user":
-            await message.channel.send(
-                "Cannot send messages to this user. This may be because this user has DMs disabled for non-friends or that admins didn't give this bot the permissions it needs.",
-            )
-        else:
-            await message.channel.send("Operation failed due to problem with permissions: " + e.text)
-    except Exception as e:
-        logger.error("Encountered error:" + str(e) + "\n" + str(traceback.format_exc()))
-        await message.channel.send("Tell <@!234561564697559041> to fix his bga bot.")
-
-
-async def trigger_bga_action(message, args):
-    author = str(message.author)
-    command = args[0][1:]
-    args.remove(args[0])
-    noninteractive_commands = ["list", "help", "options"]
-    if command in noninteractive_commands:
-        contexts[author] = {}
-    if command == "setup" and len(args) == 2:
-        bga_user, bga_passwd = args[0], args[1]
-        await setup_bga_account(message, bga_user, bga_passwd)
-    elif command == "play" and len(args) >= 2:
-        options = {}
-        game = args[0]
-        players = args[1:]
-        for arg in args:
-            if ":" in arg:
-                key, value = arg.split(":")[:2]
-                options[key] = value
-                # Options with : are not players
-                players.remove(arg)
-        discord_id = message.author.id
-        errs = await setup_bga_game(message, discord_id, game, players, options)
-        if errs:
-            await message.channel.send(errs)
-    elif command == "status" and len(args) >= 2:
-        game = ""  # if game isn't specified, then bot will search for all games
-        for arg in args:
-            if await is_game_valid(arg):
-                game = arg
-                args.remove(game)
-        players = args  # remaining args will all of the players
-        await get_tables_by_players(players, message, game_target=game)
-    elif command == "friend" and len(args) >= 2:
-        await add_friends(args[1:], message)
-    elif command == "list":
-        game_sublists = await bga_game_message_list()
-        for sublist in game_sublists:  # All messages are 1000 chars <=
-            await message.channel.send(sublist)
-    elif command == "help":
-        await send_help(message, "bga_help")
-    elif command == "options":
-        await send_help(message, "bga_options")
-    else:
-        await trigger_interactive_response(message, contexts, command, args)
-
-
-client.run(TOKEN)
+users = config.users()
+(operations, errors) = config.operations()
+print(users, operations, errors)
